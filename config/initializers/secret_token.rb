495e5893bf73527b981a234d783e2773d286ee24
Suprasanna Mishra <yknihc@gmail.com>
2013-11-24 17:35:50 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thinkful::Application.config.secret_key_base = '36e13c381f7d313da583396a0250cb619caaac769da5a515aa967d557a070f06e09b56aaca1312049df82bdc5abfcd777751c9dc82a4d869298708da97c4651c'
